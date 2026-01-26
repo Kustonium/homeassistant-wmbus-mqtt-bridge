f3a4b040d54e2fcf19420ad568e2f4d6a32fa368
Kustonium <79608504+Kustonium@users.noreply.github.com>
2026-01-26 09:10:32 +0100

    Loop Execute

--- rootfs/usr/bin/run.sh
+++ rootfs/usr/bin/run.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/with-contenv bashio
-set -e
+set -euo pipefail
 
-TOPIC=$(bashio::config 'mqtt_topic')
+TOPIC="$(bashio::config 'mqtt_topic')"
+
+MQTT_HOST="$(bashio::services mqtt 'host')"
+MQTT_PORT="$(bashio::services mqtt 'port')"
+MQTT_USER="$(bashio::services mqtt 'username')"
+MQTT_PASS="$(bashio::services mqtt 'password')"
 
 bashio::log.info "wMBus MQTT Bridge START"
-bashio::log.info "Listening on MQTT topic: $TOPIC"
+bashio::log.info "Listening on MQTT topic: ${TOPIC}"
+bashio::log.info "MQTT: ${MQTT_HOST}:${MQTT_PORT}"
 
-mosquitto_sub \
-  -h core-mosquitto \
-  -t "$TOPIC" | while read -r line; do
-    bashio::log.info "RX RAW: $line"
-done
+exec mosquitto_sub \
+  -h "${MQTT_HOST}" -p "${MQTT_PORT}" \
+  -u "${MQTT_USER}" -P "${MQTT_PASS}" \
+  -t "${TOPIC}" | while read -r line; do
+    bashio::log.info "RX RAW: ${line}"
+  done
