4bd530b402fd0cd9cb1c0c8839544e8c5cf87a64
Kustonium <79608504+Kustonium@users.noreply.github.com>
2026-02-14 18:45:29 +0100

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,9 +5,8 @@ CONF="${WMBUS_CONF:-/config/wmbusmeters.conf}"
 
 if [ ! -f "$CONF" ]; then
   echo "[ERROR] Brak pliku config: $CONF"
-  echo "        Zamontuj go jako /config/wmbusmeters.conf (volume) albo ustaw WMBUS_CONF."
   exit 2
 fi
 
 echo "[INFO] Starting wmbusmeters with config: $CONF"
-exec /usr/bin/wmbusmeters --config "$CONF"
+exec /usr/bin/wmbusmeters -c "$CONF"
